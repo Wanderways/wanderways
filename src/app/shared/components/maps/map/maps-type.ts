bb76d1291ad986a39db93532e3699b6b8ffb8326
Arno Bidet <arno.bidet479@gmail.com>
2021-11-21 11:39:36 +0100

    main[FIX] : Correction d'une faute de frappe

--- src/app/shared/components/maps/map/maps-type.ts
+++ src/app/shared/components/maps/map/maps-type.ts
@@ -10,7 +10,7 @@ export class MapsType {
     };
     public static readonly  MAP_DEPARTEMENTS_FRANCE : MapMetadata = {
         map_name :"Départements Français",
-        map_details:"Avec DotTom, hors Collectivité d'outre-mer française",
+        map_details:"Avec DomTom, hors Collectivité d'outre-mer française",
         area_identifier:"Département",
         zone_identifier :"Région"
     };
