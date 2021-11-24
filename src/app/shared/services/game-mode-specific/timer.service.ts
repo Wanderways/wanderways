95c07b32b697049032c4318b52667485d505221f
Arno Bidet <arno.bidet479@gmail.com>
2021-11-24 22:57:59 +0100

    main[add] : ] Traitement fin pour timer

--- src/app/shared/services/game-mode-specific/timer.service.ts
+++ src/app/shared/services/game-mode-specific/timer.service.ts
@@ -1,3 +1,4 @@
+import { EventListenerFocusTrapInertStrategy } from '@angular/cdk/a11y';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { GameStatusService } from 'src/app/shared/services/game-mode-specific/game-status.service';
@@ -9,57 +10,92 @@ import { GameStatus } from 'src/app/shared/utils/enums/GameStatus.enum';
 export class TimerService {
   private interval : any;
   private upperBound : number = 0;
-  private currentValue : number = 0;
-  private isEnded : boolean = false;
+  private currentValue : number = -1;
 
   private currentValueChange : Subject<number> = new Subject<number>();
+
   constructor( private gameStatusService : GameStatusService ) {
-    this.gameStatusService.getGameStatusChange().subscribe((value)=>{
-      switch(value){
-        case GameStatus.PAUSE :
-          this.pauseTimer();
-          break;
-        case GameStatus.START :
-          this.pauseTimer();
-          this.resetTimer();
-          break;
-        case GameStatus.PLAYING :          
-          this.startTimer();
-          break;
-      }
-    });
+    this.gameStatusService.getGameStatusChange().subscribe((value)=>{ this.handleGameStatusChange(value)});
   }
 
+  /**
+   * Handle the game status changes, and make the timer act as supposed to
+   * @param gameStatus 
+   */
+  handleGameStatusChange(gameStatus : GameStatus){
+    switch(gameStatus){
+      case GameStatus.PAUSE :
+        this.pauseTimer();
+        break;
+      case GameStatus.START :
+        this.pauseTimer();
+        this.resetTimer();
+        break;
+      case GameStatus.PLAYING :          
+        this.startTimer();
+        break;
+      default :
+        this.resetTimer();
+        break;
+    }
+  }
+
+  /**
+   * @TODO Make the start so that it takes a function as parameter, so that anything can be done at the end
+   */
+  /**
+   * Start the timer
+   */
   startTimer() : void {
     if(this.upperBound <=0){
       console.error("Erreur : La valeur max du timer "+(this.upperBound==0?"n'a pas été initialisée.":"est inférieur à zéro.")+" Valeur actuelle : "+this.upperBound);
     }
     this.interval = setInterval(() => {
-      if(this.currentValue > 0) {
+      if(this.currentValue > 0) { // If value is initialized, then starts counting
         this.updateValue(this.currentValue-1);
+      } else if(this.currentValue == 0){ // If timer reach 0, then pause timer then end game
+        this.pauseTimer();
+        this.gameStatusService.setGameStatus(GameStatus.GAME_END);
       } else {
         this.resetTimer();
       }
     },1000)
   }
   
-  resetTimer(){
+  /**
+   * Set the current value to the timer upper bound
+   */
+  resetTimer() : void{
     this.updateValue(this.upperBound);
   }
   
-
+  /**
+   * Pause the timer
+   */
   pauseTimer() : void{
     clearInterval(this.interval);
   }
 
+  /**
+   * Return the upper bound value
+   * @returns The upperBound value
+   */
   getUpperBound (): number{
     return this.upperBound;
   }
 
+  /**
+   * Return the current value that can be subscribed to
+   * @returns The currentValue subject
+   */
   getCurrentValueChange() : Subject<number>{
     return this.currentValueChange;
   }
 
+  /**
+   * Allow to set the timers upperbound
+   * @param upperBound 
+   */
   setUpperBound(upperBound : number) : void{
     this.upperBound = upperBound;
     this.updateValue(this.upperBound);
@@ -69,7 +105,7 @@ export class TimerService {
    * Allows to update the value of the timer
    * @param value The new value to be set
    */
-   private updateValue(value : number){
+   private updateValue(value : number) : void{
     this.currentValue = value;
     this.currentValueChange.next(value);
   }
--- src/app/shared/utils/enums/GameStatus.enum.ts
+++ src/app/shared/utils/enums/GameStatus.enum.ts
@@ -2,6 +2,7 @@ export enum GameStatus{
     START,
     PLAYING,
     PAUSE,
+    GAME_END,
     WON,
     LOST,
     ERROR
