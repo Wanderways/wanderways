b7dc9412b41f1cc745ca33d2c83474a97d963446
Arno Bidet <arno.bidet479@gmail.com>
2021-11-28 12:00:00 +0100

    main[REFA] : ordres des préfectures mises à jour

--- src/app/shared/components/maps/map-prefectures-japon/data.ts
+++ src/app/shared/components/maps/map-prefectures-japon/data.ts
@@ -2,22 +2,13 @@ import { Area } from '../../../utils/interfaces/area';
 
 export let data : Area[] = [
     {
-        "iso_3166" : "JP-23",
-        "num" : "23",
-        "name": " Aichi",
+        "iso_3166" : "JP-01",
+        "num" : "01",
+        "name": " Hokkaidō",
         "zone":{
-            "name":"Chūbu"
+            "name":"Hokkaidō"
         },
-        "city":"Nagoya"
-    },
-    {
-        "iso_3166" : "JP-05",
-        "num" : "05",
-        "name": " Akita",
-        "zone":{
-            "name":"Tōhoku"
-        },
-        "city":"Akita"
+        "city":"Sapporo"
     },
     {
         "iso_3166" : "JP-02",
@@ -29,114 +20,6 @@ export let data : Area[] = [
         "city":"Aomori"
     },
     {
-        "iso_3166" : "JP-12",
-        "num" : "12",
-        "name": " Chiba",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Chiba"
-    },
-    {
-        "iso_3166" : "JP-38",
-        "num" : "38",
-        "name": " Ehime",
-        "zone":{
-            "name":"Shikoku"
-        },
-        "city":"Matsuyama"
-    },
-    {
-        "iso_3166" : "JP-18",
-        "num" : "18",
-        "name": " Fukui",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Fukui"
-    },
-    {
-        "iso_3166" : "JP-40",
-        "num" : "40",
-        "name": " Fukuoka",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Fukuoka"
-    },
-    {
-        "iso_3166" : "JP-07",
-        "num" : "07",
-        "name": " Fukushima",
-        "zone":{
-            "name":"Tōhoku"
-        },
-        "city":"Fukushima"
-    },
-    {
-        "iso_3166" : "JP-21",
-        "num" : "21",
-        "name": " Gifu",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Gifu"
-    },
-    {
-        "iso_3166" : "JP-10",
-        "num" : "10",
-        "name": " Gunma",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Maebashi"
-    },
-    {
-        "iso_3166" : "JP-34",
-        "num" : "34",
-        "name": " Hiroshima",
-        "zone":{
-            "name":"Chūgoku"
-        },
-        "city":"Hiroshima"
-    },
-    {
-        "iso_3166" : "JP-01",
-        "num" : "01",
-        "name": " Hokkaidō",
-        "zone":{
-            "name":"Hokkaidō"
-        },
-        "city":"Sapporo"
-    },
-    {
-        "iso_3166" : "JP-28",
-        "num" : "28",
-        "name": " Hyōgo",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Kōbe"
-    },
-    {
-        "iso_3166" : "JP-08",
-        "num" : "08",
-        "name": " Ibaraki",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Mito"
-    },
-    {
-        "iso_3166" : "JP-17",
-        "num" : "17",
-        "name": " Ishikawa",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Kanazawa"
-    },
-    {
         "iso_3166" : "JP-03",
         "num" : "03",
         "name": " Iwate",
@@ -144,69 +27,6 @@ export let data : Area[] = [
             "name":"Tōhoku"
         },
         "city":"Morioka"
-    },
-    {
-        "iso_3166" : "JP-37",
-        "num" : "37",
-        "name": " Kagawa",
-        "zone":{
-            "name":"Shikoku"
-        },
-        "city":"Takamatsu"
-    },
-    {
-        "iso_3166" : "JP-46",
-        "num" : "46",
-        "name": " Kagoshima",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Kagoshima"
-    },
-    {
-        "iso_3166" : "JP-14",
-        "num" : "14",
-        "name": " Kanagawa",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Yokohama"
-    },
-    {
-        "iso_3166" : "JP-39",
-        "num" : "39",
-        "name": " Kōchi",
-        "zone":{
-            "name":"Shikoku"
-        },
-        "city":"Kōchi"
-    },
-    {
-        "iso_3166" : "JP-43",
-        "num" : "43",
-        "name": " Kumamoto",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Kumamoto"
-    },
-    {
-        "iso_3166" : "JP-26",
-        "num" : "26",
-        "name": " Kyoto",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Kyoto"
-    },
-    {
-        "iso_3166" : "JP-24",
-        "num" : "24",
-        "name": " Mie",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Tsu"
     },
     {
         "iso_3166" : "JP-04",
@@ -218,184 +38,13 @@ export let data : Area[] = [
         "city":"Sendai"
     },
     {
-        "iso_3166" : "JP-45",
-        "num" : "45",
-        "name": " Miyazaki",
+        "iso_3166" : "JP-05",
+        "num" : "05",
+        "name": " Akita",
         "zone":{
-            "name":"Kyūshū"
+            "name":"Tōhoku"
         },
-        "city":"Miyazaki"
-    },
-    {
-        "iso_3166" : "JP-20",
-        "num" : "20",
-        "name": " Nagano",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Nagano"
-    },
-    {
-        "iso_3166" : "JP-42",
-        "num" : "42",
-        "name": " Nagasaki",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Nagasaki"
-    },
-    {
-        "iso_3166" : "JP-29",
-        "num" : "29",
-        "name": " Nara",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Nara"
-    },
-    {
-        "iso_3166" : "JP-15",
-        "num" : "15",
-        "name": " Niigata",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Niigata"
-    },
-    {
-        "iso_3166" : "JP-44",
-        "num" : "44",
-        "name": " Ōita",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Ōita"
-    },
-    {
-        "iso_3166" : "JP-33",
-        "num" : "33",
-        "name": " Okayama",
-        "zone":{
-            "name":"Chūgoku"
-        },
-        "city":"Okayama"
-    },
-    {
-        "iso_3166" : "JP-47",
-        "num" : "47",
-        "name": " Okinawa",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Naha"
-    },
-    {
-        "iso_3166" : "JP-27",
-        "num" : "27",
-        "name": " Osaka",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Ōsaka"
-    },
-    {
-        "iso_3166" : "JP-41",
-        "num" : "41",
-        "name": " Saga",
-        "zone":{
-            "name":"Kyūshū"
-        },
-        "city":"Saga"
-    },
-    {
-        "iso_3166" : "JP-11",
-        "num" : "11",
-        "name": " Saitama",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Saitama"
-    },
-    {
-        "iso_3166" : "JP-25",
-        "num" : "25",
-        "name": "Shiga",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Ōtsu"
-    },
-    {
-        "iso_3166" : "JP-32",
-        "num" : "32",
-        "name": " Shimane",
-        "zone":{
-            "name":"Chūgoku"
-        },
-        "city":"Matsue"
-    },
-    {
-        "iso_3166" : "JP-22",
-        "num" : "22",
-        "name": " Shizuoka",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Shizuoka"
-    },
-    {
-        "iso_3166" : "JP-09",
-        "num" : "09",
-        "name": " Tochigi",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Utsunomiya"
-    },
-    {
-        "iso_3166" : "JP-36",
-        "num" : "36",
-        "name": " Tokushima",
-        "zone":{
-            "name":"Shikoku"
-        },
-        "city":"Tokushima"
-    },
-    {
-        "iso_3166" : "JP-13",
-        "num" : "13",
-        "name": " Tokyo",
-        "zone":{
-            "name":"Kantō"
-        },
-        "city":"Tokyo[16]"
-    },
-    {
-        "iso_3166" : "JP-31",
-        "num" : "31",
-        "name": " Tottori",
-        "zone":{
-            "name":"Chūgoku"
-        },
-        "city":"Tottori"
-    },
-    {
-        "iso_3166" : "JP-16",
-        "num" : "16",
-        "name": " Toyama",
-        "zone":{
-            "name":"Chūbu"
-        },
-        "city":"Toyama"
-    },
-    {
-        "iso_3166" : "JP-30",
-        "num" : "30",
-        "name": " Wakayama",
-        "zone":{
-            "name":"Kansai"
-        },
-        "city":"Wakayama"
+        "city":"Akita"
     },
     {
         "iso_3166" : "JP-06",
@@ -407,13 +56,112 @@ export let data : Area[] = [
         "city":"Yamagata"
     },
     {
-        "iso_3166" : "JP-35",
-        "num" : "35",
-        "name": " Yamaguchi",
+        "iso_3166" : "JP-07",
+        "num" : "07",
+        "name": " Fukushima",
         "zone":{
-            "name":"Chūgoku"
+            "name":"Tōhoku"
         },
-        "city":"Yamaguchi"
+        "city":"Fukushima"
+    },
+    {
+        "iso_3166" : "JP-08",
+        "num" : "08",
+        "name": " Ibaraki",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Mito"
+    },
+    {
+        "iso_3166" : "JP-09",
+        "num" : "09",
+        "name": " Tochigi",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Utsunomiya"
+    },
+    {
+        "iso_3166" : "JP-10",
+        "num" : "10",
+        "name": " Gunma",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Maebashi"
+    },
+    {
+        "iso_3166" : "JP-11",
+        "num" : "11",
+        "name": " Saitama",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Saitama"
+    },
+    {
+        "iso_3166" : "JP-12",
+        "num" : "12",
+        "name": " Chiba",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Chiba"
+    },
+    {
+        "iso_3166" : "JP-13",
+        "num" : "13",
+        "name": " Tokyo",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Tokyo"
+    },
+    {
+        "iso_3166" : "JP-14",
+        "num" : "14",
+        "name": " Kanagawa",
+        "zone":{
+            "name":"Kantō"
+        },
+        "city":"Yokohama"
+    },
+    {
+        "iso_3166" : "JP-15",
+        "num" : "15",
+        "name": " Niigata",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Niigata"
+    },
+    {
+        "iso_3166" : "JP-16",
+        "num" : "16",
+        "name": " Toyama",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Toyama"
+    },
+    {
+        "iso_3166" : "JP-17",
+        "num" : "17",
+        "name": " Ishikawa",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Kanazawa"
+    },
+    {
+        "iso_3166" : "JP-18",
+        "num" : "18",
+        "name": " Fukui",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Fukui"
     },
     {
         "iso_3166" : "JP-19",
@@ -423,5 +171,257 @@ export let data : Area[] = [
             "name":"Chūbu"
         },
         "city":"Kōfu"
-    }
+    },
+    {
+        "iso_3166" : "JP-20",
+        "num" : "20",
+        "name": " Nagano",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Nagano"
+    },
+    {
+        "iso_3166" : "JP-21",
+        "num" : "21",
+        "name": " Gifu",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Gifu"
+    },
+    {
+        "iso_3166" : "JP-22",
+        "num" : "22",
+        "name": " Shizuoka",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Shizuoka"
+    },
+    {
+        "iso_3166" : "JP-23",
+        "num" : "23",
+        "name": " Aichi",
+        "zone":{
+            "name":"Chūbu"
+        },
+        "city":"Nagoya"
+    },
+    {
+        "iso_3166" : "JP-24",
+        "num" : "24",
+        "name": " Mie",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Tsu"
+    },
+    {
+        "iso_3166" : "JP-25",
+        "num" : "25",
+        "name": "Shiga",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Ōtsu"
+    },
+    {
+        "iso_3166" : "JP-26",
+        "num" : "26",
+        "name": " Kyoto",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Kyoto"
+    },
+    {
+        "iso_3166" : "JP-27",
+        "num" : "27",
+        "name": " Osaka",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Ōsaka"
+    },
+    {
+        "iso_3166" : "JP-28",
+        "num" : "28",
+        "name": " Hyōgo",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Kōbe"
+    },
+    {
+        "iso_3166" : "JP-29",
+        "num" : "29",
+        "name": " Nara",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Nara"
+    },
+    {
+        "iso_3166" : "JP-30",
+        "num" : "30",
+        "name": " Wakayama",
+        "zone":{
+            "name":"Kansai"
+        },
+        "city":"Wakayama"
+    },
+    {
+        "iso_3166" : "JP-31",
+        "num" : "31",
+        "name": " Tottori",
+        "zone":{
+            "name":"Chūgoku"
+        },
+        "city":"Tottori"
+    },
+    {
+        "iso_3166" : "JP-32",
+        "num" : "32",
+        "name": " Shimane",
+        "zone":{
+            "name":"Chūgoku"
+        },
+        "city":"Matsue"
+    },
+    {
+        "iso_3166" : "JP-33",
+        "num" : "33",
+        "name": " Okayama",
+        "zone":{
+            "name":"Chūgoku"
+        },
+        "city":"Okayama"
+    },
+    {
+        "iso_3166" : "JP-34",
+        "num" : "34",
+        "name": " Hiroshima",
+        "zone":{
+            "name":"Chūgoku"
+        },
+        "city":"Hiroshima"
+    },
+    {
+        "iso_3166" : "JP-35",
+        "num" : "35",
+        "name": " Yamaguchi",
+        "zone":{
+            "name":"Chūgoku"
+        },
+        "city":"Yamaguchi"
+    },
+    {
+        "iso_3166" : "JP-36",
+        "num" : "36",
+        "name": " Tokushima",
+        "zone":{
+            "name":"Shikoku"
+        },
+        "city":"Tokushima"
+    },
+    {
+        "iso_3166" : "JP-37",
+        "num" : "37",
+        "name": " Kagawa",
+        "zone":{
+            "name":"Shikoku"
+        },
+        "city":"Takamatsu"
+    },
+    {
+        "iso_3166" : "JP-38",
+        "num" : "38",
+        "name": " Ehime",
+        "zone":{
+            "name":"Shikoku"
+        },
+        "city":"Matsuyama"
+    },
+    {
+        "iso_3166" : "JP-39",
+        "num" : "39",
+        "name": " Kōchi",
+        "zone":{
+            "name":"Shikoku"
+        },
+        "city":"Kōchi"
+    },
+    {
+        "iso_3166" : "JP-40",
+        "num" : "40",
+        "name": " Fukuoka",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Fukuoka"
+    },
+    {
+        "iso_3166" : "JP-41",
+        "num" : "41",
+        "name": " Saga",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Saga"
+    },
+    {
+        "iso_3166" : "JP-42",
+        "num" : "42",
+        "name": " Nagasaki",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Nagasaki"
+    },
+    {
+        "iso_3166" : "JP-43",
+        "num" : "43",
+        "name": " Kumamoto",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Kumamoto"
+    },
+    {
+        "iso_3166" : "JP-44",
+        "num" : "44",
+        "name": " Ōita",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Ōita"
+    },
+    {
+        "iso_3166" : "JP-45",
+        "num" : "45",
+        "name": " Miyazaki",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Miyazaki"
+    },
+    {
+        "iso_3166" : "JP-46",
+        "num" : "46",
+        "name": " Kagoshima",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Kagoshima"
+    },
+    {
+        "iso_3166" : "JP-47",
+        "num" : "47",
+        "name": " Okinawa",
+        "zone":{
+            "name":"Kyūshū"
+        },
+        "city":"Naha"
+    },
 ];
