453472ec4e1d984c98d3d9173328871f529a6064
Arno Bidet <arno.bidet479@gmail.com>
2021-11-28 11:50:01 +0100

    main[ADD] : Gestion du cas de parametres invalide pour redirection

--- src/app/core/map-page/map-page.component.ts
+++ src/app/core/map-page/map-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GameModeType } from 'src/app/shared/utils/types/game-mode.type';
 import { MapsType } from 'src/app/shared/utils/types/maps.type';
 import { MapSelection } from 'src/app/shared/utils/interfaces/map-selection.interface';
@@ -11,11 +11,12 @@ import { MapSelection } from 'src/app/shared/utils/interfaces/map-selection.inte
 })
 export class MapPageComponent implements OnInit {
 
+  displaySelection : boolean = false;
   gameModeType : GameModeType = GameModeType.GAME_INPUT;
-  mapType : MapsType = MapsType.MAP_DEPARTEMENTS_FRANCE;
+  mapType : MapsType = MapsType.MAP_UNDEFINED;
   table : boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router ) { }
 
   ngOnInit(): void {
 
@@ -27,16 +28,20 @@ export class MapPageComponent implements OnInit {
     
     // If the values are correct ones, then we process them, else we redirect to choosing page with an error snackbar
     if(MapsType.isValidType(routeParams.map) && GameModeType.isValidType(routeParams.game)){
+      this.displaySelection = false;
       // On force l'affectation car la vérification précédente assure que les données sont valides
       this.mapType = MapsType.getTypeFromIdentifier( routeParams.map)!;
       this.gameModeType = GameModeType.getTypeFromIdentifier(routeParams.game)!;
     }else if(this.route.snapshot.queryParamMap.keys.length == 0){
+      this.displaySelection = true;
       /**
        * @TODO Implements map choosing
        */
     }else{
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.router.navigate(['/maps']);
       /**
-       * @TODO Implements fail
+       * @TODO Implements error snackbar
        */
     }
   }
