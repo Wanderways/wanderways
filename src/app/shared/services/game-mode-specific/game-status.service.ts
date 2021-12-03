b333dd57e1fa427fa73717ca7fe7c9dae124b7cb
Arno Bidet <arno.bidet479@gmail.com>
2021-12-03 22:12:01 +0100

    gameModeRefactoring[REFA] : Ajout de l'accès direct à la valeur du gameStatusService

--- src/app/shared/services/game-mode-specific/game-status.service.ts
+++ src/app/shared/services/game-mode-specific/game-status.service.ts
@@ -10,12 +10,17 @@ import { GameModeMetaData } from '../../utils/interfaces/game-oriented/game-mode
 })
 export class GameStatusService {
 
+  private currentGameStatus : GameStatus = GameStatus.START;
   private gameStatusChange : Subject<GameStatus> = new Subject<GameStatus>();
 
   constructor() {
     this.gameStatusChange.next(GameStatus.START);
   }
   
+  public getGameStatus(){
+    return this.currentGameStatus;
+  }
+
   /**
    * Allows to obtain the subject GameStatus to subcribe to its changes
    * @returns 
@@ -25,6 +30,7 @@ export class GameStatusService {
   }
 
   public setGameStatus(gameStatus : GameStatus){
+    this.currentGameStatus = gameStatus;
     this.gameStatusChange.next(gameStatus);
   }
 
