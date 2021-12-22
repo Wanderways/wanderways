bbf491517a962292c3dfdbd440e938836793bb9a
Arno Bidet <arno.bidet479@gmail.com>
2021-12-22 23:53:10 +0100

    main[ADDREFA] : déplacement du controller de jeu

--- src/app/shared/components/maps-games/maps-games.module.ts
+++ src/app/shared/components/maps-games/maps-games.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FindAreaComponent } from './find-area/find-area.component';
 import { MaterialModule } from '../../../material.module';
 import { FormsModule } from '@angular/forms'; 
-import { GameControllerComponent } from './utilitary/game-controller/game-controller.component';
+import { GameControllerComponent } from './game-controller/game-controller.component';
 
 
 let games : any[] = [
