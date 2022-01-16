1aef60c68cbc046d2151174052c3851235982685
Arno Bidet <arno.bidet479@gmail.com>
2022-01-16 10:50:54 +0100

    main[ADD] : Comportement du header, relative sur les jeux, sticky sinon

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, HostBinding } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -15,10 +17,34 @@ export class HeaderComponent implements OnInit {
   @Input() displaySideNav : boolean | undefined = false
   @Output() displaySideNavChange = new EventEmitter();
 
+  /**
+   * List of path where the header should be set as relative
+   */
+  relativeHeaderOnRoutesList : string[] = [
+    "/maps/games/*"
+  ];
+  /**
+   * True if the header position property should be set as `relative`, false if `stycky`
+   */
+  @HostBinding('class.relativeHeader') relativeHeader: boolean = false;
 
-  constructor() { }
+  constructor(private router : Router) {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => e as NavigationEnd) // We only want to have one of the many navigation events
+    ).subscribe(e => this.onRouteChange(e));
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Check the current url, if needed, set the position property to `sticky` or `relative` through class change
+   * @param param0 The current url
+   */
+  onRouteChange({ url } : NavigationEnd){
+    this.relativeHeader = this.relativeHeaderOnRoutesList.some((path : string) =>url.match(new RegExp(path)));
   }
 
   onDarkModeSwitched({checked}:MatSlideToggleChange){
