b12f90cd8c98b1f468215a62806d393232c7b73f
Arno Bidet <arno.bidet479@gmail.com>
2022-03-12 22:09:19 +0100

    main[REFA] : Changement du logo, de component a simple svg

--- src/app/shared/atomic/atoms/atoms.module.ts
+++ src/app/shared/atomic/atoms/atoms.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LogoComponent } from './logo/logo.component';
 
 let components : any[] = [
-  LogoComponent
 ]
 
 @NgModule({
