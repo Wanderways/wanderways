a63d8aee689d188020a76ef9ce3380b6d4252547
Arno Bidet <arno.bidet479@gmail.com>
2022-03-17 21:15:54 +0100

    main[REFA] : Diminution du bundle par suppression de modules inutiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AtomicModule } from './shared/atomic/atomic.module';
-import { CoreModule } from './core/core.module';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAnalytics,getAnalytics,ScreenTrackingService } from '@angular/fire/analytics';
 import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import {MatRippleModule} from '@angular/material/core';
+import { CoreModule } from './core/core.module';
 
 let matModules : any[]=[
-  BrowserAnimationsModule,
-  MatButtonModule,
-  MatIconModule,
   MatRippleModule
 ]
 
@@ -29,11 +22,10 @@ let matModules : any[]=[
     AppComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
     AtomicModule,
     CoreModule,
-    NoopAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
     ...matModules,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
