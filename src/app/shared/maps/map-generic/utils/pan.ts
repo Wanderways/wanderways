3dc4bc00a58945756269fa4a429e8bdaab51fc26
Arno Bidet <arno.bidet479@gmail.com>
2022-03-31 22:57:51 +0200

    testCanvas[REFA] : Panning 100% fonctionnel et commentaires

--- src/app/shared/maps/map-generic/utils/pan.ts
+++ src/app/shared/maps/map-generic/utils/pan.ts
@@ -1,21 +1,27 @@
 import { applyTransformMatrix } from "./applyTransformMatrix";
-import { convertScreenCoordsToSvgCoords } from "./convertScreenCoordsToSvgCoords";
 import { Coordinates } from "./coordinates";
 
-export function pan(panParameters :PanParameters) : DOMMatrix{
+/**
+ * Pan the given element following the mouse depending on current transform
+ * @param param0 The 
+ * @returns The transformed matrice
+ */
+export function pan({ currentTransform, containerElement, toPanElement, deltaCoordinates }: PanParameters): DOMMatrix {
     // Get the mouse position as SVG coordinates
-    panParameters.currentTransform = panParameters.currentTransform.translate( panParameters.deltaCoordinates.x / panParameters.currentTransform.a ,  panParameters.deltaCoordinates.y / panParameters.currentTransform.a);
-    
+    const containerElementRect = containerElement.getClientRects().item(0)!;
+    const containerElementViewBox = containerElement.viewBox.baseVal;
+    currentTransform = currentTransform.translate(
+        (deltaCoordinates.x / containerElementRect.width * containerElementViewBox.width / currentTransform.a),
+        (deltaCoordinates.y / containerElementRect.height * containerElementViewBox.height / currentTransform.a));
     // Now we need to update the group's transform
-    var transform = panParameters.containerElement.createSVGTransform()!;        // An SVGTransform DOM object...
-    applyTransformMatrix(transform, panParameters.toPanElement, panParameters.currentTransform);
-    return  panParameters.currentTransform;
+    var transform = containerElement.createSVGTransform()!;        // An SVGTransform DOM object...
+    applyTransformMatrix(transform, toPanElement, currentTransform);
+    return currentTransform;
 }
 
-export interface PanParameters{
-    currentTransform : DOMMatrix,
-    containerElement : SVGSVGElement,
-    toPanElement : SVGGElement,
-    coordinates : Coordinates,
-    deltaCoordinates : Coordinates
+export interface PanParameters {
+    currentTransform: DOMMatrix,
+    containerElement: SVGSVGElement,
+    toPanElement: SVGGElement,
+    deltaCoordinates: Coordinates
 }
