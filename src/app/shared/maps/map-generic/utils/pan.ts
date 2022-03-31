fc9afff36e2e313a3b5d192bec2d276d78d843a6
Arno Bidet <arno.bidet479@gmail.com>
2022-03-31 23:08:45 +0200

    testCanvas[FIX] : Scroll y correct sur mobile

--- src/app/shared/maps/map-generic/utils/pan.ts
+++ src/app/shared/maps/map-generic/utils/pan.ts
@@ -3,16 +3,19 @@ import { Coordinates } from "./coordinates";
 
 /**
  * Pan the given element following the mouse depending on current transform
- * @param param0 The 
+ * @param param0 The params necessary to calculate the panning
  * @returns The transformed matrice
  */
 export function pan({ currentTransform, containerElement, toPanElement, deltaCoordinates }: PanParameters): DOMMatrix {
     // Get the mouse position as SVG coordinates
     const containerElementRect = containerElement.getClientRects().item(0)!;
     const containerElementViewBox = containerElement.viewBox.baseVal;
+    // Ration to counter the potential height that makes parent container takes more space than usual, breakin svg ratio
+    const ratioHeightPerWidth = containerElementViewBox.height/containerElementViewBox.width;
+    
     currentTransform = currentTransform.translate(
         (deltaCoordinates.x / containerElementRect.width * containerElementViewBox.width / currentTransform.a),
-        (deltaCoordinates.y / containerElementRect.height * containerElementViewBox.height / currentTransform.a));
+        (deltaCoordinates.y / containerElementRect.width / ratioHeightPerWidth * containerElementViewBox.height / currentTransform.a));
     // Now we need to update the group's transform
     var transform = containerElement.createSVGTransform()!;        // An SVGTransform DOM object...
     applyTransformMatrix(transform, toPanElement, currentTransform);
