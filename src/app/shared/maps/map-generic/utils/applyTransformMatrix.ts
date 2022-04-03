cf64c7c4fe1f5fd21082a078ec444b104ef1731a
Arno Bidet <arno.bidet479@gmail.com>
2022-04-03 22:40:22 +0200

    testCanvas[REFA] : Amélirotion lisibilité de code et commentaire

--- src/app/shared/maps/map-generic/utils/applyTransformMatrix.ts
+++ src/app/shared/maps/map-generic/utils/applyTransformMatrix.ts
@@ -1,3 +1,9 @@
+/**
+ * Apply a matrix to an element
+ * @param svgTransform The transform to apply
+ * @param svgGElement The svg on which to apply the transforms
+ * @param newMatrix The parents matrix newly generated
+ */
 export function applyTransformMatrix(svgTransform : SVGTransform, svgGElement : SVGGraphicsElement, newMatrix : DOMMatrix){
     svgTransform.setMatrix(newMatrix); // set to the new groupTransform...
     svgGElement.transform.baseVal.initialize(svgTransform); // and used to update the circle transform property
