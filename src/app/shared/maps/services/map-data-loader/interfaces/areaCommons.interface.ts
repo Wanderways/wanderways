8eadeb990874bccac99e25eab54f31784c43d688
Arno Bidet <arno.bidet479@gmail.com>
2022-04-04 20:06:57 +0200

    testCanvas[ADD] : Modification data. Ajout capitals par région

--- src/app/shared/maps/services/map-data-loader/interfaces/areaCommons.interface.ts
+++ src/app/shared/maps/services/map-data-loader/interfaces/areaCommons.interface.ts
@@ -2,6 +2,7 @@ import { Locale_I18n } from "../../../utils/locale_i18n.interface";
 
 export interface AreaCommons{
     identifier : string,
+    numeric_code:string,
     name: Locale_I18n,
     group:string
 }
