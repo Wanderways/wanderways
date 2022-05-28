38e1b67c633b3efb382f78ba092133ce78c30a86
Arno Bidet <arno.bidet479@gmail.com>
2022-05-28 09:51:28 +0200

    main[ADD] : Ajout de liens sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,10 @@ import { Component,  HostListener } from '@angular/core';
 })
 export class AppComponent {
 
-  displaySideNav : boolean | undefined = undefined;
+  displaySideNav : boolean = window.innerWidth > 1024;
   isScrolled : boolean = false;
+  navLinks : NavLink[]= navLink;
+  footerLinks: NavLink[]= footerLinks;
 
   /**
    * On scroll, if not top page, then blur header
@@ -25,3 +27,105 @@ export class AppComponent {
     this.displaySideNav = !this.displaySideNav;
   }
 }
+
+
+
+interface NavLink {
+  isLink : boolean,
+  link : string,
+  icon : string,
+  label : string,
+  isDisabled : boolean,
+  catLink : {
+    link : string,
+    label : string,
+    isDisabled : boolean
+  }[]
+}
+
+const navLink : NavLink[] = [
+  {
+    isLink : true,
+    link : "/",
+    icon : "./assets/icons/home.svg",
+    label : $localize `:{sidenav-home}:Home`,
+    isDisabled : false,
+    catLink :[]
+  },
+  {
+    isLink : false,
+    link : "",
+    icon : "./assets/icons/binoculars.svg",
+    label : $localize `:{sidenav-maps}:Maps`,
+    isDisabled : false,
+    catLink :[
+      {
+        link : "/maps",
+        label : $localize `:{sidenav-all-maps}:All maps`,
+        isDisabled : false
+      },
+      {
+        link : "/favorites-map",
+        label : $localize `:{sidenav-favorites-map}:Favorites`,
+        isDisabled : true
+      }
+    ]
+  },
+  {
+    isLink : false,
+    link : "",
+    icon : "./assets/icons/flag.svg",
+    label : $localize `:{sidenav-games}:Games`,
+    isDisabled : false,
+    catLink :[
+      {
+        link : "/games",
+        label : $localize `:{sidenav-all-games}:All games`,
+        isDisabled : true
+      },
+      {
+        link : "/favorites-game",
+        label : $localize `:{sidenav-favorites-game}:Favorites`,
+        isDisabled : true
+      }
+    ]
+  },
+  
+  {
+    isLink : false,
+    link : "",
+    icon : "./assets/icons/bookmark.svg",
+    label : $localize `:{sidenav-news-and-stuff}:News and stuff`,
+    isDisabled : false,
+    catLink :[
+      {
+        link : "/last-news",
+        label : $localize `:{sidenav-last-news}:Last news`,
+        isDisabled : true
+      },
+      {
+        link : "/roadmap",
+        label : $localize `:{sidenav-roadmap}:Roadmap`,
+        isDisabled : true
+      }
+    ]
+  }];
+const footerLinks : NavLink[] = [{
+  isLink : false,
+  link : "",
+  icon : "/assets/icons/logo.svg",
+  label : "Learn your maps",
+  isDisabled : false,
+  catLink :[
+    {
+      link : "/learn-more",
+      label : $localize `:{sidenav-learn-more}:Learn more`,
+      isDisabled : true
+    },
+    {
+      link : "/announces",
+      label : $localize `:{sidenav-announces}:Announces`,
+      isDisabled : true
+    }
+  ]
+}];
