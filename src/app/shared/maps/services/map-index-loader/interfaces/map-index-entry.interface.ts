f7af61a8a341ca608409b96f0aab0893c2bbe3f2
Arno Bidet <arno.bidet479@gmail.com>
2022-06-21 00:57:27 +0200

    main[REFA] : Change data structure et noms fichiers

--- src/app/shared/maps/services/map-index-loader/interfaces/map-index-entry.interface.ts
+++ src/app/shared/maps/services/map-index-loader/interfaces/map-index-entry.interface.ts
@@ -3,10 +3,11 @@ import { Locale_I18n } from '../../../utils/locale_i18n.interface';
 export interface MapIndexEntry{
         mapIdentifier : string,
         mapFlagUrl : string,
-        relatedSvg : string,
-        mapDisplayName : Locale_I18n,
-        map_type: string,
         tagIdList:string[],
-        wiki_url:string,
-        available:boolean
+        mapDisplayName : Locale_I18n,
+        maps:{
+                identifier:string,
+                mapDisplayName:Locale_I18n
+                wiki_url:string
+        }[]
 }
