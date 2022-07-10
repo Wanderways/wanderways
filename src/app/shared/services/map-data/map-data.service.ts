0248ccfe7d98467950738795ee622c0a50523d97
Arno Bidet <arno.bidet479@gmail.com>
2022-07-11 00:14:53 +0200

    againstClock[REFA] : REfa AreaCommons -> MapData

--- src/app/shared/services/map-data/map-data.service.ts
+++ src/app/shared/services/map-data/map-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AreaCommons } from '../../interfaces/areaCommons.interface';
+import { MapData } from '../../interfaces/MapData.interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +11,7 @@ export class MapDataService {
   
   constructor(private http: HttpClient) { }
 
-  getMapMetaData(map_identifier : string): Observable<AreaCommons[]>{
-    return this.http.get<AreaCommons[]>(this.PATH+map_identifier+this.FILE_EXTENSION);
+  getMapData(map_identifier : string): Observable<MapData[]>{
+    return this.http.get<MapData[]>(this.PATH+map_identifier+this.FILE_EXTENSION);
   }
 }
