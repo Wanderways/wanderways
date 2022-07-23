6f97bbcc6ae7a71bf8732e7fbc15e419f2323582
Arno Bidet <arno.bidet479@gmail.com>
2022-07-23 17:13:18 +0200

    againstClock[REFA] : Locali18n changement pour accepter key any

--- src/app/shared/interfaces/locale_i18n.interface.ts
+++ src/app/shared/interfaces/locale_i18n.interface.ts
@@ -1,7 +1,8 @@
 export interface Locale_I18n{
     en_US:string[],
-    fr_FR?:string[],
-    de_DE?:string[],
-    es_ES?:string[],
-    pt_PT?:string[]
+    fr_FR:string[],
+    de_DE:string[],
+    es_ES:string[],
+    pt_PT:string[],
+    [key: string]:string[]
 }
