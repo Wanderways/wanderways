9594b7ff683de4f181acb6fad0a9ccc16e7b6647
Arno Bidet <arno.bidet479@gmail.com>
2022-07-23 17:13:48 +0200

    againstClock[REFA] : Timer commence à 100%

--- src/app/shared/components/timer/timer.component.ts
+++ src/app/shared/components/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import { AdjustingInterval } from '../../utils/AdjustingInterval';
 @Component({
   selector: 'app-timer',
@@ -9,8 +9,8 @@ export class TimerComponent implements OnInit {
   @Input('timerDuration') timerDuration: number = 300;
   @Output('onCurrentValueChanges') currentValue: number = 0
   currentReadableValue: string = "";
-  currentValueAsPercentage = 50;
-  RAYON=90;
+  currentValueAsPercentage = 100;
+  RAYON = 90;
   CIRCUMFERENCE = Math.PI * 2 * this.RAYON;
   currentStage: string = "";
   stages = [{ percentage: 30, color: "red" }, { percentage: 60, color: "orange" }, { percentage: 100, color: "var(--map-land-found)" }];
@@ -25,15 +25,20 @@ export class TimerComponent implements OnInit {
 
   ngOnInit(): void {
     this.resetTimer();
-    this.startTimer();
-    this.currentValue = this.timerDuration;
-    this.currentReadableValue = this.toFormat(this.timerDuration);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.timerDuration)
+      if (changes.timerDuration.currentValue !== 0)
+        this.startTimer();
   }
 
   /**
    * Starts the timer
    */
   startTimer() {
+    this.currentValue = this.timerDuration;
+    this.currentReadableValue = this.toFormat(this.timerDuration);
     this.ticker?.start();
   }
 
@@ -48,10 +53,14 @@ export class TimerComponent implements OnInit {
    * Reset the timer
    */
   resetTimer() {
+    this.currentStage = this.stages.find(e => e.percentage >= this.getAsPercentage())!.color;
+    this.currentValueAsPercentage = this.getAsPercentage();
+
     this.ticker = new AdjustingInterval(() => {
       this.currentValue--;
       this.currentReadableValue = this.toFormat(this.currentValue);
       this.currentStage = this.stages.find(e => e.percentage >= this.getAsPercentage())!.color;
+      this.currentValueAsPercentage = this.getAsPercentage();
       if (this.currentValue === 0) {
         this.stopTimer();
       }
@@ -64,8 +73,8 @@ export class TimerComponent implements OnInit {
 
   /**
    * Return a time in a mm:ss format
-   * @param seconds 
-   * @returns 
+   * @param seconds
+   * @returns
    */
   toFormat(seconds: number): string {
     return new Date(seconds * 1000).toISOString().substr(14, 5);
