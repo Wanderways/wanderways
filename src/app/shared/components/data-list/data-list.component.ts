5179db20a0c5c03983634cbf475457d3a8adaa56
Arno Bidet <arno.bidet479@gmail.com>
2022-07-27 00:32:59 +0200

    againstClock[ADD] : Communication exterieur evenement data list

--- src/app/shared/components/data-list/data-list.component.ts
+++ src/app/shared/components/data-list/data-list.component.ts
@@ -1,43 +1,45 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, IterableDiffers, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+
 import { MapData } from '../../interfaces/MapData.interface';
-import { MapDataService } from '../../services/map-data/map-data.service';
 
 @Component({
-  selector: 'app-data-list',
+  selector: 'app-data-list[originList]',
   templateUrl: './data-list.component.html',
   styleUrls: ['./data-list.component.scss']
 })
 export class DataListComponent implements OnInit {
+  @ViewChild('dataContainer') dataContainer!: ElementRef;
   @Input('maxHeight') maxHeight: string | undefined;
-  @Input('mapIdentifier') mapIdentifier: string | undefined;
-  data: MapData[] = [];
-  constructor(private mapDataService: MapDataService) { }
-  ngOnInit(): void {
-    console.log("yeah", this.mapIdentifier);
-    if (this.mapIdentifier)
-      this.loadMapData(this.mapIdentifier);
-  }
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.mapIdentifier) this.onMapIdentifierChange(changes.mapIdentifier.currentValue);
-  }
+  @Input('inGame') inGame: true | undefined = undefined;
+  @Input('originList') originList: MapData[] = [];
+  @Input('foundList') foundList: MapData[] = [];
+  @Input('currentSelected') currentSelected: MapData | undefined = undefined;
+  @Output('onSelectedRow') selectedRowEmmitter: EventEmitter<MapData> = new EventEmitter<MapData>();
 
-  /**
-   * Loads maps svg and related data
-   */
-  onMapIdentifierChange(mapIdentifier: string) {
-    if (!mapIdentifier) return; // If there is no value then skip
-    this.loadMapData(mapIdentifier);
+  constructor(private iterableDiffers: IterableDiffers) { }
+  ngOnInit(): void { }
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    if (simpleChanges.currentSelected) if (simpleChanges.currentSelected.currentValue) this.onCurrentSelected(simpleChanges.currentSelected.currentValue)
   }
 
-  loadMapData(mapIdentifier: string) {
-    console.log("yeah");
-    
-    if (this.mapIdentifier)
-      this.mapDataService.getMapData(mapIdentifier).subscribe(e => {
-        this.data = e;
-        console.log(e);
+  onCurrentSelected(mapData: MapData) {
+    this.scrollToRow(mapData);
+  }
 
-      })
+  onSelectedRow(mapData: MapData) {
+    this.selectedRowEmmitter.emit(mapData);
+    this.scrollToRow(mapData)
+  }
 
+  isDataFound(mapData: MapData): boolean {
+    return !!this.foundList.find(e => e.identifier === mapData.identifier);
+  }
+  scrollToRow(mapData: MapData) {
+    this.dataContainer.nativeElement.scrollTo(0, document.getElementById('data-list-row-' + mapData.identifier)?.offsetTop);
+  }
+
+  isRowDisplayable(mapData: MapData): boolean {
+    return !this.inGame || (this.inGame && this.isDataFound(mapData));
   }
 }
