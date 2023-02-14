c5f2eb0ef8d425d4bd41260766e36eaf1e05202a
Arno Bidet <arno.bidet479@gmail.com>
2023-02-14 22:19:17 +0100

    againstClock[REFA] : Renommage du projet dans les fichiers de configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learn-your-maps app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wanderways app is running!');
   });
 });
