b650d934fa532e648ad91b14708b818aacc7dc12
Arno Bidet <arno.bidet479@gmail.com>
2023-02-21 20:33:54 +0100

    main[TEST] : Création test pour app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component : AppComponent;
+  let fixture : ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,19 +15,33 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render main-content div', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wanderways app is running!');
+    expect(compiled.querySelector('div#main-content')).toBeTruthy();
+  });
+
+  it('should render the sidenav component', () => {
+    const sidenavElement = fixture.debugElement.query(By.css('app-sidenav'));
+    expect(sidenavElement.nativeElement).toBeTruthy();
+  });
+
+  it('should render the header component', () => {
+    const headerElement = fixture.debugElement.query(By.css('app-header'));
+    expect(headerElement.nativeElement).toBeTruthy();
+  });
+
+  it('should render the router outlet', () => {
+    const routerOutletElement = fixture.debugElement.query(By.css('router-outlet'));
+    expect(routerOutletElement.nativeElement).toBeTruthy();
   });
 });
