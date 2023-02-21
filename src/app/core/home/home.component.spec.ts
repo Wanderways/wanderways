682423ec6f09659989d6c23b90865e8fc5691b24
Arno Bidet <arno.bidet479@gmail.com>
2023-02-21 21:29:42 +0100

    main[TEST] : Création test pour home

--- src/app/core/home/home.component.spec.ts
+++ src/app/core/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
 
@@ -22,4 +23,20 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should render a section",()=>{
+    const sectionDe = fixture.debugElement.query(By.css('section#home-presentation-text'));
+    expect(sectionDe.nativeElement).toBeTruthy();
+  })
+
+  it("should render a welcome message",()=>{
+    const title : HTMLElement = fixture.debugElement.query(By.css('section#home-presentation-text>h1#home-welcome')).nativeElement;
+    expect(title.textContent).toMatch(/Welcome to Wanderways/i);
+  })
+
+  it("should render a catch phrase",()=>{
+    const catchPhrase : HTMLElement = fixture.debugElement.query(By.css('section#home-presentation-text>span#home-catch-phrase')).nativeElement;
+    expect(catchPhrase.textContent).toMatch(/To learn maps the fun way/i);
+  })
+
 });
